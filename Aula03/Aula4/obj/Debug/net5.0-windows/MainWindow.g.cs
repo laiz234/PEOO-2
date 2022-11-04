60f48dbeddc30614b47c62eb86cdea48907d7050
LAIZ FERNANDA SOUZA DOS SANTOS <laiz.santos@academico.ifrn.edu.br>
2022-11-04 15:48:47 -0300

    aula0411

--- Aula03/Aula4/obj/Debug/net5.0-windows/MainWindow.g.cs
+++ Aula03/Aula4/obj/Debug/net5.0-windows/MainWindow.g.cs
@@ -101,7 +101,7 @@ namespace Aula4 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Aula4;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Aula4_be5wnp15_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
